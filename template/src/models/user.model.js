7f0dce8f3e30a84fd56c009260dae4a78a772ef7
Gajendra Sharma <sharmagajendra167@gmail.com>
2026-09-16 00:37:05 +0530

    update the userSchema

--- template/src/models/user.model.js
+++ template/src/models/user.model.js
@@ -1,28 +1,36 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        required: [true, 'username is required'],
-        minlength: [3, 'username must be more then of 3 characters'],
-        maxlength: [40, 'username cannot be more then of 40 character long'],
-        trim: true
+      type: String,
+      required: [true, 'Username is required'],
+      minlength: [3, 'Username must be at least 3 characters long'],
+      maxlength: [40, 'Username cannot exceed 40 characters'],
+      trim: true,
     },
     email: {
-        type: String,
-        required: [true, 'email is required'],
-        unique: [true, 'email must be unique'],
-        match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/, 'Please fill a valid email address'],
-        lowercase: true,
-        trim: true
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+      match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/, 'Please provide a valid email address'],
+      lowercase: true,
+      trim: true,
     },
     password: {
-        type: String,
-        required: [true, 'password is required'],
-        match: [/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]).*$/, 'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character'],
-        minlength: [8, 'password must be atleast of 8 character long'],
-        select: false
-    }
-})
+      type: String,
+      required: [true, 'Password is required'],
+      minlength: [8, 'Password must be at least 8 characters long'],  //we will validate the password requirement in controller because Password regex breaks bcrypt hashes
+      select: false, // Prevents sensitive password leakage in general queries
+    },
+    refreshToken: {
+      type: String,
+      select: false, // Prevents sensitive token leakage in general queries
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const userModel = mongoose.model('users', userSchema)
+export const User = mongoose.model('User', userSchema);
